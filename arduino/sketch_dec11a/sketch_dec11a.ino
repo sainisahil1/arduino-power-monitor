9880756295c99aeb5017a6d03b91b834dae9960b
Sahil <sahils123saini@gmail.com>
2024-12-16 22:17:35 +0100

    mqtt connection

--- arduino/sketch_dec11a/sketch_dec11a.ino
+++ arduino/sketch_dec11a/sketch_dec11a.ino
@@ -1,50 +1,61 @@
-#include "thingProperties.h"
 
-// float total_current;
+#include "secrets.h"
+#include <ArduinoMqttClient.h>
+#include <WiFi.h>
 
-// const char WIFI_SSID[] = "FRITZ!Box 7520 QB";
-// const char WIFI_PASSWORD[] = "02100898750741008823";
+WiFiSSLClient espClient;
+MqttClient mqttClient(espClient);
+
+void connectToMQTT() {
+  while (!mqttClient.connected()) {
+    Serial.print("Connecting to MQTT...");
+    if (mqttClient.connect(PUBSUB_ENDPOINT, PORT)){
+      Serial.println("Connected to MQTT broker");
+    } else {
+      Serial.print("Failed, rc=");
+      Serial.print(mqttClient.connectError());
+      Serial.println(" Trying again in 5 seconds...");
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-
+  
   Serial.begin(115200);
   delay(1500); 
-
-  initProperties();
-
-  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
-
-  // ArduinoCloud.addProperty(total_current, READ, ON_CHANGE, NULL);
-
-  // WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   delay(3000);
-  //   Serial.print(".");
-  //   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  // }
-  // Serial.println("WiFi connected!");
-  // Serial.println(WiFi.localIP());
-
-  // ArduinoCloud.begin(ArduinoIoTPreferredConnection(WIFI_SSID, WIFI_PASSWORD));
-
-  // total_current = 0.0;
-
-  setDebugMessageLevel(2);
-  ArduinoCloud.printDebugInfo();
-
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to Wi-Fi!");
+  mqttClient.setUsernamePassword(USERNAME, PASS);
+  mqttClient.setKeepAliveInterval(300000);
+  connectToMQTT();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  ArduinoCloud.update();
 
-  total_current = random(1000) / 100.0;
+  mqttClient.poll();
+
+  float total_current = random(1000) / 100.0;
+  float led_current = random(1000) / 100.0;
+  float motor_current = random(1000) / 100.0;
 
   Serial.print("Sending sensor value: ");
-  Serial.println(total_current);
 
-  
+  char payload[100];
+  snprintf(payload, sizeof(payload), "{\"total_current\": %.2f, \"led_current\": %.2f, \"motor_current\": %.2f}", total_current, led_current, motor_current);
+
+  Serial.println(payload);
+  bool retained = false;
+    int qos = 1;
+    bool dup = false;
+
+    mqttClient.beginMessage(TOPIC, sizeof(payload), retained, qos, dup);
+    mqttClient.print(payload);
+    mqttClient.endMessage();
 
   delay(5000);
 
