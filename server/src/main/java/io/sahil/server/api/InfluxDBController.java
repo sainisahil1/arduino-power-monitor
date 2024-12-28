74c81b020845d78704d432e8df868dbff83ce983
Sahil <sahils123saini@gmail.com>
2024-12-28 23:44:45 +0100

    include cross origin

--- server/src/main/java/io/sahil/server/api/InfluxDBController.java
+++ server/src/main/java/io/sahil/server/api/InfluxDBController.java
@@ -6,6 +6,7 @@ import io.sahil.server.util.HistoryType;
 import io.sahil.server.util.QueryType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ public class InfluxDBController {
         this.repository = repository;
     }
 
+    @CrossOrigin
     @GetMapping("/data")
     public InfluxDTO getData(
             @RequestParam(value = "time", defaultValue = "daily") String time,
